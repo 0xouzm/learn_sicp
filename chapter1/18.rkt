19931e91aa0e65ee6c7a65a30df3547c81e19cd5
ouzm <wjj315315@gmail.com>
2020-07-12 16:40:43 +0800

    1.18

--- chapter1/18.rkt
+++ chapter1/18.rkt
@@ -1,28 +1,22 @@
 #lang sicp
-(define (fast-expt b n)
-  (expt-iter b n 1))
-
-(define (expt-iter b n a)
-  (cond 
-    ((= n 0) a)
-    ((even? n) 
-    (expt-iter (* b b) (/ n 2) a))
-    ((odd? n)
-    (expt-iter b (dec n) (* b a))))
-  )
-
 (define (double x)
 (* 2 x))
 
 (define (halve x)
 (/ x 2))
 
-(define (* a b)
-  (cond ((= b 0) 0)
-    ((even? b) 
-    (double (* a (halve b))))
-    (else 
-    (+ a (* a (dec b))))))
+(define (multi a b)
+(multi-iter a b 0)
+)
 
+(define (multi-iter a b product)
+(cond ((= b 0) product)
+  ((even? b) (multi-iter (double a) (halve b) product
+  ))
+  ((odd? b)
+  
+    (multi-iter a (dec b) (+ a product))
+)))
 
-()
+(multi 2 2)
+(multi 3 5)
